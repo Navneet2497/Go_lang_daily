1e9703e9cd690ae5ef399eaff607b0d1700f1842
Navneet2497 <79110256+Navneet2497@users.noreply.github.com>
2024-05-24 00:59:32 +0530

    golang Variables_concepts

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello navneet")
+
+	var navneet string = "money"
+	var version = 89
+
+	fmt.Println(navneet)
+	fmt.Println(version)
+
+}
